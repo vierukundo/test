238e0673569df116dd19d7870c4609bcf8840fd1
vierukundo <vierukundo@gmail.com>
2023-12-21 02:57:36 +0200

    Added search bar

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,7 @@ import os
 from app.models.machinery import Machinery
 from app.models.material import Material
 from app.models.manpower import Manpower
+from urllib.parse import quote, unquote
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4', 'avi'}
 
@@ -19,14 +20,14 @@ def allowed_file(filename):
                            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', strict_slashes=False)
 def index():
     images_folder = '/home/rukundo/test/app/static/images'
     images = os.listdir(images_folder)
     images_path = [f'images/{image}' for image in images if image.startswith('profile')]
     return render_template('homepage.html', title='Home Page', images=images_path)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'], strict_slashes=False)
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -44,12 +45,12 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/logout')
+@app.route('/logout', strict_slashes=False)
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'], strict_slashes=False)
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -62,7 +63,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/reset_password_request', methods=['GET', 'POST'])
+@app.route('/reset_password_request', methods=['GET', 'POST'], strict_slashes=False)
 def reset_password_request():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -76,7 +77,7 @@ def reset_password_request():
         return redirect(url_for('login'))
     return render_template('reset_password_request.html', title='Reset Password', form=form)
 
-@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+@app.route('/reset_password/<token>', methods=['GET', 'POST'], strict_slashes=False)
 def reset_password(token):
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -91,93 +92,112 @@ def reset_password(token):
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
 
-@app.route('/materials')
-def materials():
+@app.route('/materials', strict_slashes=False)
+@app.route('/materials/<query>', strict_slashes=False)
+def materials(query=None):
     from app.models import storage
     from app.models.material import Material
-    materials = storage.all(Material)
-    sorted_materials = sorted(materials.values(), key=lambda x: x.category)
+    materials_dict = storage.all(Material)
+    materials = materials_dict.values()
+    
+    match_objects = []
+
+    if query:
+        for obj in materials:
+            # Iterate over all attributes of the object
+            for attribute_name in dir(obj):
+                attribute_value = getattr(obj, attribute_name)
+                # Check if the query matches the attribute value
+                if isinstance(attribute_value, str) and query.lower() in attribute_value.lower():
+                    match_objects.append(obj)
+        materials = match_objects
+    sorted_materials = sorted(materials, key=lambda x: x.category)
     categories = []
     for material in sorted_materials:
         if material.category not in categories:
             categories.append(material.category)
     return render_template('material.html', materials=sorted_materials, categories=categories)
 
-@app.route('/machineries')
-def machineries():
+@app.route('/machineries', strict_slashes=False)
+@app.route('/machineries/<query>', strict_slashes=False)
+def machineries(query=None):
     from app.models import storage
     from app.models.machinery import Machinery
-    machineries = storage.all(Machinery)
-    sorted_machineries = sorted(machineries.values(), key=lambda x: x.category)
-    print(sorted_machineries)
+    machineries_dict = storage.all(Machinery)
+    machineries = machineries_dict.values()
+    
+    match_objects = []
+
+    if query:
+        for obj in machineries:
+            # Iterate over all attributes of the object
+            for attribute_name in dir(obj):
+                attribute_value = getattr(obj, attribute_name)
+                # Check if the query matches the attribute value
+                if isinstance(attribute_value, str) and query.lower() in attribute_value.lower():
+                    match_objects.append(obj)
+        machineries = match_objects
+    sorted_machineries = sorted(machineries, key=lambda x: x.category)
     categories = []
     for machinery in sorted_machineries:
         if machinery.category not in categories:
             categories.append(machinery.category)
-    print(categories)
     return render_template('machinery.html', machineries=sorted_machineries, categories=categories)
 
-@app.route('/management')
+@app.route('/management', strict_slashes=False)
 def management():
     return render_template('management.html')
 
-@app.route('/money')
+@app.route('/money', strict_slashes=False)
 def money():
     return render_template('money.html')
 
-@app.route('/manpowers')
-def manpowers():
+@app.route('/manpowers', strict_slashes=False)
+@app.route('/manpowers/<query>', strict_slashes=False)
+def manpowers(query=None):
     from app.models import storage
     from app.models.manpower import Manpower
-    manpowers = storage.all(Manpower)
-    sorted_manpowers = sorted(manpowers.values(), key=lambda x: x.profession)
+    manpowers_dict = storage.all(Manpower)
+    manpowers = manpowers_dict.values()
+
+    match_objects = []
+
+    if query:
+        for obj in manpowers:
+            # Iterate over all attributes of the object
+            for attribute_name in dir(obj):
+                attribute_value = getattr(obj, attribute_name)
+                # Check if the query matches the attribute value
+                if isinstance(attribute_value, str) and query.lower() in attribute_value.lower():
+                    match_objects.append(obj)
+        manpowers = match_objects
+    sorted_manpowers = sorted(manpowers, key=lambda x: x.profession)
     categories = []
     for manpower in sorted_manpowers:
         if manpower.profession not in categories:
             categories.append(manpower.profession)
     return render_template('manpower.html', manpowers=sorted_manpowers, categories=categories)
 
-@app.route('/search_results')
+@app.route('/search_results', strict_slashes=False)
 def search_results():
     query = request.args.get('query')
     page_name = request.args.get('page')
     classes = {
-            'manpower': [Manpower, 'manpower.html', 'profession'],
-            'machinery': [Machinery, 'machinery.html', 'category'],
-            'material': [Material, 'material.html', 'category']
+            'manpower': [Manpower, 'manpowers'],
+            'machinery': [Machinery, 'machineries'],
+            'material': [Material, 'materials']
             }
     class_data = classes.get(page_name, None)
-    class_name = class_data[0]
-    if class_name and query:
-        from app.models import storage
-        objs = storage.all(class_name)
-        print(query)
-        category = class_data[2]
 
-        def attribute_match(obj):
-            # Iterate over all attributes of the object
-            for attribute_name in dir(obj):
-                attribute_value = getattr(obj, attribute_name)
-                # Check if the query matches the attribute value
-                if isinstance(attribute_value, str) and query.lower() in attribute_value.lower():
-                    return attribute_value
-                return None
-        sorted_objs = sorted(objs.values(), key=attribute_match)
-        print(sorted_objs)
-        categories = []
-        for obj in sorted_objs:
-            category_name = getattr(obj, category, None)
-            if category_name not in categories:
-                categories.append(category_name)
-        return render_template(class_data[1], manpowers=sorted_objs, categories=categories)
-    return {page_name: query}
+    return redirect(url_for(class_data[1], query=query))
 
-@app.route('/dashboard')
+        
+@app.route('/dashboard', strict_slashes=False)
 @login_required
 def dash():
     return render_template('dashboard.html')
 
-@app.route('/add_manpower', methods=['GET', 'POST'])
+@app.route('/add_manpower', methods=['GET', 'POST'], strict_slashes=False)
 def add_manpower():
     form = ManpowerEntryForm()
     if form.validate_on_submit():
@@ -211,7 +231,7 @@ def add_manpower():
         return redirect(url_for('dash'))
     return render_template('add_manpower.html', form=form)
 
-@app.route('/add_material', methods=['GET', 'POST'])
+@app.route('/add_material', methods=['GET', 'POST'], strict_slashes=False)
 def add_material():
     form = MaterialEntryForm()
 
@@ -250,7 +270,7 @@ def add_material():
 
     return render_template('add_material.html', form=form)
 
-@app.route('/add_machinery', methods=['GET', 'POST'])
+@app.route('/add_machinery', methods=['GET', 'POST'], strict_slashes=False)
 def add_machinery():
     form = MachineryEntryForm()
 
@@ -289,7 +309,7 @@ def add_machinery():
 
     return render_template('add_machinery.html', form=form)
 
-@app.route('/add_money', methods=['GET', 'POST'])
+@app.route('/add_money', methods=['GET', 'POST'], strict_slashes=False)
 def add_money():
     form = MoneyEntryForm()
     if form.validate_on_submit():
